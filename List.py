346c5f584f51b60896efdf66c1f2940d8611165d
hariom936 <hariomverma968@gmail.com>
2023-10-28 00:21:07 +0530

    Basic of Types of Algo Built-in

---
+++ List.py
@@ -0,0 +1,15 @@
+''' 
+List is collection of 0 or many elements seprated by ' , ' 
+and closed in squar bracket .
+'''
+
+List = [1,3,4]
+print("This is your", List)
+
+List1 = [1,2.3,"hello"]
+print("This List1 is store data of different Data Type",List1)
+
+List == List1 # compaer both list
+
+List2 =[[1,2],3,4]
+print("This LIst2 is nested List",List2)
